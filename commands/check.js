91cd19ea5931e662c02f7009ba27d08699b133ab
Mr.Sithum Eshan <156440091+SithumEshan45@users.noreply.github.com>
2024-02-12 20:13:27 +0530

    Update check.js

--- commands/check.js
+++ commands/check.js
@@ -6,6 +6,6 @@ const maker = require('mumaker')
 cmd({ pattern: "check", alias: ["testbot"], category: "textpro", react: "✅", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
         if (!text) return citel.react("✅")
         let anu = await maker.textpro('https://fdown.net/download.php', Url)
-        Void.sendMessage(citel.chat, { video: { url: anu }, caption: `⦿.*𝗠𝗔𝗗𝗘 𝗕𝗬 :-* ${tlang().title} 🧚 ${tlang().greet}` }, { quoted: citel })
+        Void.sendMessage(citel.chat, { video: { url: anu }, caption: `⦿.*𝗠𝗔𝗗𝗘 𝗕𝗬 :- ⃞🌷𝗠𝗥.𝗦𝗜𝗧𝗛𝗨𝗠 𝗘𝗦𝗛𝗔𝗡💙⃝* ${tlang().title} 🧚 ${tlang().greet}` }, { quoted: citel })
     })
                            
